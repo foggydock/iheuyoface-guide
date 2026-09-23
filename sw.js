096d61dfaa58d3d2a4b4cfcb0f3f3b098d7beef7
wokanon <okanoy1@gmail.com>
2026-09-24 06:46:29 +0900

    Make guidebook installable on mobile

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'iheuyoface-guide-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './site.webmanifest', './icon.png', './icon512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
